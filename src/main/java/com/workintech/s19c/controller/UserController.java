d22685989c0439500a93d1015df74e2db843cfbc
Anıl Teoman <anilteoman@outlook.com>
2025-09-16 14:04:00 +0300

    Create UserController.java

---
+++ src/main/java/com/workintech/s19c/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.workintech.s19c.controller;
+
+
+import com.workintech.s19c.entity.User;
+import com.workintech.s19c.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+}
