fdc6a80b7d913b46b93c36481e954ecb5526405e
Anıl Teoman <anilteoman@outlook.com>
2025-09-16 14:04:03 +0300

    Create UserService.java

---
+++ src/main/java/com/workintech/s19c/service/UserService.java
@@ -0,0 +1,24 @@
+package com.workintech.s19c.service;
+
+
+import com.workintech.s19c.entity.User;
+import com.workintech.s19c.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
